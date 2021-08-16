b30a492953704e97fb2bf3df88ebe05ae9cfe912
unknown <jacobandrey971@gmail.com>
2021-08-16 16:53:15 +1000

    change home link

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,7 +4,7 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://www.nsolar.com/'
+    href: 'https://www.nsolarfarmdefi.com/'
   },
   {
     label: 'Trade',
